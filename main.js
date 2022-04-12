2787c7813ae82dd05a5933771951062d6a5a8f96
jrzen6972 <flightcommand13@gmail.com>
2022-04-12 19:43:21 -0400

    Tidied code; Comments

--- main.js
+++ main.js
@@ -3,29 +3,14 @@ apiKey = 'dc0b3a0b8ee54077aa4e71f03e600aef'
 url = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/A01?api_key=' + apiKey
 stationUrl = "https://api.wmata.com/Rail.svc/json/jStations?api_key=" + apiKey
 
-let pieArray = [];
-
-//colors identical to WMATA
-let lineColors = {
-	'RD': [191, 13, 62],
-	'BL': [0, 156, 222],
-	'OR': [237, 139, 0],
-	'SV': [145, 157, 157],
-	'GR': [0, 177, 64],
-	'YL': [255, 209, 0],
-	'No': "gray"
-}
-
-//let locationDict = {}
 function preload() {
-	stationData = loadJSON(stationUrl)
-	askWMATA()
+	stationData = loadJSON(stationUrl) //run 1 time request for stations
+	askWMATA()	//get info for trains
 
+	//loads fonts
 	HNB = loadFont("fonts/HelveticaNeue Bold.ttf")
 	HNM = loadFont("fonts/HelveticaNeue Medium.ttf")
 }
-
-
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -35,20 +20,22 @@ function setup() {
 
 	stations = stationData["Stations"]
 
+	//dropdown menu
 	sel = createSelect();
 	sel.style("background", "gray")
 	sel.style("font-family", "Arial")
 	sel.style("font-size", width / 46 + "px")
 	sel.center()
 	sel.style("text-align", "center")
-  
 	sel.size(width * 0.55, height * 0.059)
-  if(width>height){
-   	sel.position(width / 2 - sel.width / 2, height / 2 + sel.height * 2)
-  }else{
-    sel.position(width / 2 - sel.width / 2, height / 2 + (height/6) + sel.height * 2)
-  }
+	if(width>height){
+		sel.position(width / 2 - sel.width / 2, height / 2 + sel.height * 2)
+	}else{
+		sel.position(width / 2 - sel.width / 2, height / 2 + (height/6) + sel.height * 2)
+	}
+	sel.changed(updateUrl)
 
+	//Populate Location Dictionary and Dropdown Box
 	for (let i = 0; i < stations.length; i++) {
 		if (stations[i].Name == "Metro Center" || stations[i].Name == "Gallery Pl-Chinatown" || stations[i].Name == "L'Enfant Plaza" || stations[i].Name == "Fort Totten") {
 			locationDict.create(stations[i].Name + i, stations[i].Code)
@@ -59,32 +46,33 @@ function setup() {
 		}
 	}
 
-	sel.changed(updateUrl)
-
 	textAlign(CENTER)
 	angleMode(DEGREES)
 	textFont(HNB)
 
-	setInterval(askWMATA, 1500)
+	setInterval(askWMATA, 1500) //request every 1.5 seconds
 
-	if (width > height) {
+	if (width > height) { //horizontal / desktop view
+		//scaling
 		xOrigin = (width / 40 + width * 0.065)
 		xMulti = (width / 12 + width * 0.08)
 		y = height / 2.75
 
+		//populate pieArray
 		for (let i = 0; i < allTrains.length; i++) {
 			pieArray[i] = new chartClass(xOrigin + xMulti * i, y)
 		}
-
-	} else {
+	} else { //mobile view
+		//scaling
 		xOrigin = (width / 5)
 		xMulti = (width / 4 + width * 0.04)
 		y = height / 3 - 45
 		y2 = height / 1.83
 
+		//populate pieArray
 		for (let i = 0; i < allTrains.length / 2; i++) {
-			pieArray[i] = new chartClass(xOrigin + xMulti * i, y)
-			pieArray[i + 3] = new chartClass(xOrigin + xMulti * i, y2)
+			pieArray[i] = new chartClass(xOrigin + xMulti * i, y) //upper level
+			pieArray[i + 3] = new chartClass(xOrigin + xMulti * i, y2) //lower level
 		}
 	}
 
@@ -93,15 +81,15 @@ function setup() {
 function draw() {
 	background(28)
 
-	timeElement()
+	timeElement() //display clock
 
 	if (allTrains.length != 0) {
 		for (let i = 0; i < allTrains.length; i++) {
-			pieArray[i].display(allTrains, i)
+			pieArray[i].display(allTrains, i) //show train dots(inputting train data)
 		}
 	
 		for(let i = 0; i<allTrains.length;i++){
-			pieArray[i].textPopUp()
+			pieArray[i].textPopUp()	//ensure popups are above all dots
 		}
 	}
 }
