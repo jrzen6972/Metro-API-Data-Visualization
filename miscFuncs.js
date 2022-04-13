e321dcf92be74ce52d3c0cdb04a5cef51f67857d
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2022-04-13 10:42:07 -0400

    Update (not working atm)

--- miscFuncs.js
+++ miscFuncs.js
@@ -16,8 +16,64 @@ function askWMATA() {
 	loadJSON(url, gotData, 'json')
 }
 
+// function gotData(data) {
+// 	allTrains = data["Trains"]
+// }
+
+// function newFunc(array){
+//     return sortedArray
+// }
+
+// Stuff ain't working...
+
 function gotData(data) {
 	allTrains = data["Trains"]
+	newFunc(allTrains)
 }
 
-// test to jeff-branch
+const groupData = [
+	{group: "2"},
+	{group: "1"},
+	{group: "2"},
+	{group: "1"},
+	{group: "2"},
+	{group: "1"}
+];
+
+function newFunc(array){
+	// url = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/' + apiKey
+	// loadJSON(url, gotData, 'json')
+
+	let sortedArray = groupData.groupBy( ({ group }) => group );
+
+    return sortedArray
+}
+
+
+// function gotData(data) {
+//     allTrains = data["Trains"]
+//     sortedArray(allTrains)
+// }
+
+// const groupData = [
+// 	{"Group":"2"},
+// 	{"Group":"1"},
+// 	{"Group":"2"},
+// 	{"Group":"1"},
+// 	{"Group":"2"},
+// 	{"Group":"1"}
+// ];
+
+// function sortedArray(array){
+// 	let result = groupData.groupBy( ({ Group }) => Group );
+//     return sortedArray
+// };
+
+// const groupData = [
+// 	{"DestinationName":"Shady Grove","Group":"2"},
+// 	{"DestinationName":"Glenmont","Group":"1"},
+// 	{"DestinationName":"Shady Grove","Group":"2"},
+// 	{"DestinationName":"Glenmont","Group":"1"},
+// 	{"DestinationName":"Shady Grove","Group":"2"},
+// 	{"DestinationName":"Glenmont","Group":"1"}
+// ];
