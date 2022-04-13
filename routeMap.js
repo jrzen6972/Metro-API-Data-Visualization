e0bbd0f7c4c8599a386b4e9e64232565f925c364
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2022-04-13 10:42:07 -0400

    Update

--- routeMap.js
+++ routeMap.js
@@ -1,43 +1,49 @@
-apiKey = 'bce8f914a243405e80a73d82e0de2d2b'
-pathUrl = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=N06&ToStationCode=G05&api_key=' + apiKey
 // https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=N06&ToStationCode=G05&api_key=bce8f914a243405e80a73d82e0de2d2b
+apiKey = 'bce8f914a243405e80a73d82e0de2d2b'
+pathUrl_RD = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=A15&ToStationCode=B11&api_key=' + apiKey
+pathUrl_BL = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=J03&ToStationCode=G05&api_key=' + apiKey
+pathUrl_YL = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=E10&ToStationCode=C15&api_key=' + apiKey
+pathUrl_OR = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=K08&ToStationCode=D13&api_key=' + apiKey
+pathUrl_GR = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=E10&ToStationCode=F11&api_key=' + apiKey
+pathUrl_SV = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=N06&ToStationCode=G05&api_key=' + apiKey
 
+stationURL = 'https://api.wmata.com/Rail.svc/json/jStations?api_key=' + apiKey
 
 // The following code is inspired by this: https://editor.p5js.org/Poupas/sketches/IH-UuIUT
 
-let counter; // Change
+let counter; // Change to show live feed of train's pos.
 
 function preload() {
-	stationData = loadJSON(pathUrl)
-	
-	// Fonts, come back later
-	// HNB = loadFont("HelveticaNeue Bold.ttf")
-	// HNM = loadFont("HelveticaNeue Medium.ttf")
+	pathData = loadJSON(pathUrl_RD) // Set to Red Line
+	stationData = loadJSON(stationURL)
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth/2, windowHeight/2);
 
 	if(width<height){
 		mobile = true
 	}
+	
+	stations = stationData['StationName']
 	
   counter = new Count(0,100)
   counter.start();
 }
 
 function stationPaths() {
-	routeMap = 	stationData['Path'];
+	routeMap = 	pathData['Path'];
 	
 	// Shows the two-letter abbreviation for the line (e.g.: RD, BL, YL, OR, GR, or SV) this station's platform is on.
-	// Currently, API is set to ONLY show the Silver Line (SV)
-	lineCode = stationData['Path'][0].LineCode 
+	lineCode = pathData['Path'][0].LineCode 
 	
-	// Full name or code for this station, as shown on the WMATA website.
-	stationCode1 = stationData['Path'][0].StationCode
-	stationCode2 = stationData['Path'][3].StationCode	// stationName or stationCode from StationName
+	// Full name (stationName) or code (stationCode) for this station, as shown on the WMATA website.
+	// stationCode1 = pathData['Path'][0].StationCode
+	// stationCode2 = pathData['Path'][27].StationCode
+	stationName1 = pathData['Path'][0].StationName
+	stationName2 = pathData['Path'][10].StationName
 	
-	distancePrev = stationData['Path'][3].DistanceToPrev // Undef as well. Need to find two locations
+	distancePrev = pathData['Path'][10].DistanceToPrev // Future: calculate distance between two stations the user selects. 
 }
 
 function draw() {
@@ -51,7 +57,8 @@ function draw() {
   fill(0,255,0);
   textSize(32);
   textFont('monospace')
-  let txt = text('Distance between '+ stationCode1 + ' and ' + stationCode2 + ': '+ distancePrev + ' feet.',0,middle-20); // See stationCode/stationName above in stationPaths()
+	let info_text = text('Current Metro Line Selected: ' + lineCode, 0, middle-100);
+  let distance_txt = text('Distance between '+ stationName1 + ' and ' + stationName2 + ': '+ distancePrev + ' feet.',0,middle-20); // See stationCode/stationName above in stationPaths()
   
   rect(0,middle,Progress,20,15)
   stroke(0,255,0)
