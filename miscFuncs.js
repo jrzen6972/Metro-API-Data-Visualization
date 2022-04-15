54e742c9e7a58223febcf90ecf0e9de1b8e89a8f
jrzen6972 <flightcommand13@gmail.com>
2022-04-14 20:59:52 -0400

    Removing dampen for now

--- miscFuncs.js
+++ miscFuncs.js
@@ -34,15 +34,3 @@ locCode = locationDict.get(findClosest().Name)
 url = 'https://api.wmata.com/StationPrediction.svc/json/GetPrediction/' + locCode + '?api_key=' + apiKey
 askWMATA()
 }
-
-const dampingFactor = 0.05;
-
-damper = Damper();
-
-function Damper(val) {
-    this.val = 0;
-    function f(val) {
-        return this.val += dampingFactor * (val - this.val);
-    }
-    return f;
-}
