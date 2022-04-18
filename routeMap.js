bfbbc01d5aa37ca0bd9896e4df7ab586be2d7d71
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2022-04-18 12:56:39 -0400

    Update

--- routeMap.js
+++ routeMap.js
@@ -1,7 +1,7 @@
-// apiKey = 'bce8f914a243405e80a73d82e0de2d2b'
-// pathUrl = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=N06&ToStationCode=G05?api_key=' + apiKey
+apiKey = 'bce8f914a243405e80a73d82e0de2d2b'
+pathUrl = 'https://api.wmata.com/Rail.svc/json/jPath?FromStationCode=N06&ToStationCode=G05&api_key=' + apiKey
 
-pathUrl = 'https://developer.wmata.com/docs/services/5476364f031f590f38092507/export?DocumentFormat=Swagger'
+// pathUrl = 'https://developer.wmata.com/docs/services/5476364f031f590f38092507/export?DocumentFormat=Swagger'
 
 let counter;
 
@@ -25,16 +25,16 @@ function setup() {
 }
 
 function stationPaths() {
-	routeMap = 	stationData['MetroPathItem'];
+	routeMap = 	stationData['Path'];
 	
 	// Shows the two-letter abbreviation for the line (e.g.: RD, BL, YL, OR, GR, or SV) this station's platform is on.
-	lineCode = stationData['MetroPathItem'].LineCode 
+	lineCode = stationData['Path'].LineCode 
 	
 	// Full name for this station, as shown on the WMATA website.
-	stationName1 = stationData[3]['MetroPathItem'].StationName
-	stationName2 = stationData[4]['MetroPathItem'].StationName
+	stationName1 = stationData['Path'].StationName // Current issues: is undefined. perhaps need a [] between stationData and ['Path']
+	stationName2 = stationData['Path'].StationName
 	
-	distancePrev = stationData['MetroPathItem'].DistanceToPrev
+	distancePrev = stationData['Path'].DistanceToPrev
 }
 
 function draw() {
@@ -82,3 +82,93 @@ class Count{
     this.s = 0
   }
 }
+
+// The following code is inspired by this: https://editor.p5js.org/Poupas/sketches/IH-UuIUT
+
+// let counter;
+// function setup() {
+//   createCanvas(400, 400);
+//   counter = new Count(0,100)
+//   counter.start();
+// }
+
+// function draw() {
+//   background(0);
+//   let middle = height/2;
+//   let sVal = counter.s;
+//   let Progress = map(sVal,0,100,0,width);
+  
+//   fill(0,255,0);
+//   textSize(32);
+//   textFont('monospace')
+//   let txt = text('Progress : '+ sVal + '%',0,middle-20);
+  
+//   rect(0,middle,Progress,20,15)
+//   stroke(0,255,0)
+//   noFill();
+//   rect(0,middle,width,20,15)
+  
+//   if (floor(random(300)) == 100) {
+//     counter.reset();
+//   }
+  
+// }
+
+// class Count{
+//   constructor(s,w){
+//     this.s = s
+//     this.w = w
+//     this.p = createP('')
+//   }
+//   start(){
+//     if (!this.done) {
+//       setInterval(() => this.counter(),this.w)
+//     }
+//   }
+//   counter(){
+//     if(this.s < 100){
+//       this.s ++
+//       this.p.html(this.s)
+//     }
+//   }
+//   reset(){
+//     this.s = 0
+//   }
+// }
+
+
+// Below is older code from somewhere else, probably won't use.
+
+// int startTime; int counter; int maxTime; 
+// boolean = done; 
+
+// function setup() { //setup function called initially, only once 
+// 	size(250, 250); background(255); 
+	
+// 	//set background white // colorMode(HSB); //set colors to Hue, Saturation, Brightness mode 
+// 	counter = 0; startTime= millis(); maxTime=random(2000,8001); done=false;
+
+// } 
+
+// function draw() { //draw function loops 
+// 	background(244); 
+// 	if (counter[startTime] < maxTime) {
+// 		counter=millis();
+// 	}
+	
+// 	else {
+// 		done=true;
+// 	}
+	
+// 	fill(244,3,3);
+// 	noStroke();
+// 	rect(20,100,map(counter[startTime], 0, maxTime, 0, 200), 19);
+// 	text(counter- startTime+" " + int(maxTime) +  " " + int (map(counter[startTime], 0, maxTime, 0, 200)), 20, 160);
+// 	noFill();
+// 	stroke(0);
+// 	rect(20,100,200,19);
+//  }
+
+// function mousePressed () { if (done) { counter = 0; startTime= millis();
+
+//  maxTime=random(2000,8001); done=false; } }
