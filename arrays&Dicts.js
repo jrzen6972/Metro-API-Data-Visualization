29837d51475bcdb1fcecda898c0777f64da555ef
jrzen6972 <flightcommand13@gmail.com>
2022-04-21 22:51:01 -0400

    addition of month dictionary

--- arrays&Dicts.js
+++ arrays&Dicts.js
@@ -51,3 +51,18 @@ let lineInfo = {
 	'No': "gray",
 	'--': "gray"
 }
+
+let monthDict = {
+	"January": 1, 
+	"February": 2,
+	"March": 3, 
+	"April": 4, 
+	"May": 5, 
+	"June": 6,
+	"July": 7,
+	"August": 8,
+	"September": 9,
+	"October": 10,
+	"November": 11,
+	"December": 12
+}
