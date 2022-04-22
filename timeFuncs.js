98af18bded420b9af3b7d60aa6ea16a316f67fa8
jrzen6972 <flightcommand13@gmail.com>
2022-04-21 22:51:53 -0400

    removed push,pop,translate. added nm()

--- timeFuncs.js
+++ timeFuncs.js
@@ -1,7 +1,4 @@
 function timeElement() {
-	push()
-	translate(0, height / 7)
-
 	stroke(0)
 	textAlign(CENTER)
 	fill("white")
@@ -12,33 +9,33 @@ function timeElement() {
 	if (minute() < 10) {
 		currentTime[1] = "0" + minute()
 	}
-	//keep clock 12 hour
-	if (hour() == 12 || hour() == 0) {
-		currentTime[0] = 12
-	}
 
 	//display time
 	if (mobile) { //mobile scale
 		textSize(width * 0.17)
-		text(currentTime[0] + ":" + currentTime[1], width / 2, 0)
+		text(nM(currentTime[0]) + ":" + currentTime[1], width / 2, 0)
 		translate(width / 4.75 + width * 0.08, 0)
 		textSize(width * 0.09)
-		text(amPM(), width / 2, 0)
+		text(amPM(hour()), width / 2, 0)
 	} else { //desktop scale
 		textSize(width * 0.06)
-		text(currentTime[0] + ":" + currentTime[1],
-			width / 2, 0)
-		translate(width / 11, 0)
-		textSize(width * 0.035)
-		text(amPM(), width / 2 + 30, 0)
+		text(nM(currentTime[0]) + ":" + currentTime[1] + "\n"+ amPM(hour()),
+			width *.9, height/2)
 	}
-	pop()
 }
 
-function amPM() {
-	if (hour() >= 12) {
-		return "PM"
+function amPM(hour) {
+	if (hour >= 12) {
+		return " PM"
 	} else {
-		return "AM"
+		return " AM"
+	}
+}
+
+function nM(hour){
+	if (hour == 12 || hour == 0) {
+		return 12
+	}else{
+		return hour
 	}
 }
